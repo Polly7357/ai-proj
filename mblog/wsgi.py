1216af124973d4bd6fecec13677a2524b22af230
Polly <polly.huang@gmail.com>
2023-08-15 12:59:49 +0800

    update wsgi.py

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -19,7 +19,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 import sys      # 20230815
 
-path = "/home/polly.huang/mblog/"   # 20230815
+path = "/home/pollyhuang/mblog/"   # 20230815
 
 if path not in sys.path:            # 20230815
     sys.path.insert(0, path)        # 20230815
