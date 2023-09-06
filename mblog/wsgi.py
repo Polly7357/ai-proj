a9afd33a7eec6eb3c5a6af2ad44567ffaef532b2
Polly <polly.huang@gmail.com>
2023-09-06 10:26:24 +0800

    update path wsgi.py

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -19,10 +19,17 @@ import os
 from django.core.wsgi import get_wsgi_application
 import sys      # 20230815
 
-path = "/home/pollyhuang/mblog/"   # 20230815
+# path = "/home/pollyhuang/mblog/"   # 20230815
 
-if path not in sys.path:            # 20230815
-    sys.path.insert(0, path)        # 20230815
+# if path not in sys.path:            # 20230815
+#     sys.path.insert(0, path)        # 20230815
+
+path = "/home/pollyhuang/django_env/lib/python3.10/site-packages" # 整段20230906
+if path not in sys.path:
+    sys.path.insert(0, path)
+path = "/home/pollyhuang/myAssistant"
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mblog.settings")
