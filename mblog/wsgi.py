739fe742561074b54318d0209e25c30064212cc2
Polly <polly.huang@gmail.com>
2023-09-06 11:00:52 +0800

    update wsgi.py v2

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -27,7 +27,7 @@ import sys      # 20230815
 path = "/home/pollyhuang/django_env/lib/python3.10/site-packages" # 整段20230906
 if path not in sys.path:
     sys.path.insert(0, path)
-path = "/home/pollyhuang/myAssistant"
+path = "/home/pollyhuang/mblog"
 if path not in sys.path:
     sys.path.insert(0, path)
 
