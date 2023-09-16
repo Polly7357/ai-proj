c005afc5d0523ae5cb3156fffaa538fdb570b20e
Polly <polly.huang@gmail.com>
2023-09-16 16:23:49 +0800

    working Views V.1.1

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 from mainsite.models import *
 import json, os
 import sqlite3
@@ -27,61 +28,96 @@ def calculate_electricity_cost_view(request):
     # try:
     #     # Read JSON data from the request (you may need to adjust this based on how you send JSON data to the view)
     #     #json_data = json.loads(request.body)
+    cases = ['Reality','Smart']
+    case_usage={}
 
-    try:
-        # Check if "user_data.json" file exists      
-        json_file_path = "suggest_user_data.json"
-        if not os.path.exists(json_file_path):
-            return JsonResponse({"error": "File 'user_data.json' does not exist"}, status=400)
-        # else:
-        #     print('file exist.')
+    for case in cases:
+        if case == 'Reality':
+            json_file_path = "user_data_house_perday.json"
 
-        # Check if "user_data.json" file is empty
-        if os.path.getsize(json_file_path) == 0:
-            return JsonResponse({"error": "File 'user_data.json' is empty"}, status=400)
-        # else:
-        #     print('file is not empty.')
-
-        # Read JSON data from the "user_data.json" file
-        with open(json_file_path, "r", encoding="utf-8") as json_file:
-            json_data = json.load(json_file)
-        #    print(json_data)
+        elif case == 'Smart':
+            json_file_path = "suggest_user_data.json"
 
 
-
-        tables = ["c2_rates", "summer_c2_rates", "c3_rates", "summer_c3_rates"]
-
-        for table in tables:
-            electricity, electricity_cost, wend_cost = calculate_electricity_cost(json_data, 'db.sqlite3', table)
-            monthly_consumption, monthly_costs = monthly_usage(electricity, electricity_cost, wend_cost)
-            if electricity_cost is not None:
-                #print(f"\nTable: {table}")
-                #print(f"單日用電約: {electricity:.2f} kWh; 單日電費約 {electricity_cost:.2f} 元; 週末電費約 {wend_cost:.2f} 元")
-                #print(f'月估用電約: {monthly_consumption:.2f} 度; 月電費約 {monthly_costs:.2f} 元')
-                #print(f'使用表燈累進式費率, 月電費約 2469 元')
-                            # Prepare the response data
-                electricity = round(electricity,2)
-                electricity_cost = round(electricity_cost,2)
-                wend_cost = round(wend_cost,2)
-                monthly_consumption = round(monthly_consumption,2)
-                monthly_costs =round(monthly_costs,2)
-
-                response_data = {
-                    "單日估算電量(度)": electricity,
-                    "單日估算電費(元)": electricity_cost,
-                    "週末電費(元)": wend_cost,
-                    "整月估計電量(度)": monthly_consumption,
-                    "整月估計電費(元)": monthly_costs,
-                }
+        try:
+            # Check if "user_data.json" file exists      
+            if not os.path.exists(json_file_path):
+                return JsonResponse({"error": "File does not exist"}, status=400)
             else:
-                #print(f"Unable to calculate electricity cost for {table}.")
-                return JsonResponse({"error": str(e)}, status=400)
+                print(json_file_path,'file exist.')
 
-            return JsonResponse(response_data, status=200)
+            # Check if "user_data.json" file is empty
+            if os.path.getsize(json_file_path) == 0:
+                return JsonResponse({"error": "File is empty"}, status=400)
+            else:
+                print(json_file_path,'file is not empty.')
 
-    except Exception as e:
-        print("An error occurred:", str(e))
-        return JsonResponse({"error orrurs": str(e)}, status=400)
+            # Read JSON data from the "user_data.json" file
+            with open(json_file_path, "r", encoding="utf-8") as json_file:
+                json_data = json.load(json_file)
+            #    print(json_data)
+
+
+            tables = ["c2_rates", "c3_rates", "summer_c2_rates", "summer_c3_rates"]
+            usage_data={}
+            usage_list={}
+            user_id = json_data['id']
+            cal_time = round(datetime.now().timestamp())
+
+            for table in tables:
+                electricity, electricity_cost, wend_cost = calculate_electricity_cost(json_data, 'db.sqlite3', table)
+                monthly_consumption, monthly_costs = monthly_usage(electricity, electricity_cost, wend_cost)
+                
+                match table:
+                    case 'c2_rates':
+                        pricing = '二段式'
+                    case 'c3_rates':
+                        pricing = '三段式'
+                    case 'summer_c2_rates':
+                        pricing = '二段式夏季'                    
+                    case 'summer_c3_rates':
+                        pricing = '三段式夏季'                    
+                    
+                if electricity_cost is not None:
+                    #print(f"\nTable: {table}")
+                    #print(f"單日用電約: {electricity:.2f} kWh; 單日電費約 {electricity_cost:.2f} 元; 週末電費約 {wend_cost:.2f} 元")
+                    #print(f'月估用電約: {monthly_consumption:.2f} 度; 月電費約 {monthly_costs:.2f} 元')
+                    #print(f'使用表燈累進式費率, 月電費約 2469 元')
+                                # Prepare the response data
+                    electricity = round(electricity,2)
+                    electricity_cost = round(electricity_cost,2)
+                    wend_cost = round(wend_cost,2)
+                    monthly_consumption = round(monthly_consumption,2)
+                    monthly_costs =round(monthly_costs,2)
+
+                    usage_data[pricing] = {
+                        "單日估算電費(元)": electricity_cost,
+                        "週末電費(元)": wend_cost,
+                        "整月估計電費(元)": monthly_costs,
+                    }
+                    #usage_list.append(usage_data)
+
+                else:
+                    #print(f"Unable to calculate electricity cost for {table}.")
+                    return JsonResponse({"error": str(e)}, status=400)
+                
+            case_usage[case]=usage_data 
+                
+
+
+        except Exception as e:
+            print("An error occurred:", str(e))
+            return JsonResponse({"error orrurs": str(e)}, status=400)
+        
+    response_data ={
+    "來源": user_id,
+    "產生時間": cal_time,
+    "單日估算電量(度)": electricity,
+    "整月估計電量(度)": monthly_consumption,
+    "case": case_usage,
+    }
+
+    return JsonResponse(response_data, status=200)
 
 
 
